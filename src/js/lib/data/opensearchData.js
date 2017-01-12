c8a26f97e4d73fa1aada377d4d67adae16507ebb
Dimitar Danov <dimitardanov@users.noreply.github.com>
2017-01-12 16:36:07 +0200

    Added wikipedia opensearch query settings data

---
+++ src/js/lib/data/opensearchData.js
@@ -0,0 +1,12 @@
+
+
+var wikiOpensearchData = {
+  action: 'opensearch',
+  limit: 20,
+  redirects: 'resolve',
+  format: 'json',
+  formatversion: 2,
+  search: ''
+};
+
+module.exports = wikiOpensearchData;
