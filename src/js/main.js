0813dd3b41d5de652c18d11f00160a48715fae48
Dimitar Danov <dimitardanov@users.noreply.github.com>
2017-01-13 23:04:57 +0200

    Added the main.js func to run the search

--- src/js/main.js
+++ src/js/main.js
@@ -4,5 +4,56 @@
 
 
 $(function() {
-  console.log('hello world');
+  var osData = require('./lib/data/opensearchData.js');
+  var search = require('./lib/ajax/wikiSearch.js');
+  var helpers = require('./lib/helpers/helpers.js');
+  var logFuncs = require('./lib/helpers/ajaxResponseLogic.js');
+  var renderResults = require('./lib/render/renderResults');
+  var renderNoResultsMsg = require('./lib/render/renderNoResultsMsg.js');
+
+  var wikiURL = 'https://en.wikipedia.org/w/api.php';
+
+  var $searchBtn = $('#search-btn');
+  var $query = $('#query');
+
+  function renderResultsWrapper (data) {
+    var template = require('./lib/templates/opensearchResult.hbs');
+    var $container = $('#results');
+    $container.html('');
+    return renderResults(data, template, $container);
+  }
+
+  function renderNoResultsMsgWrapper (query) {
+    var template = require('./lib/templates/noResultsMsg.hbs');
+    var $container = $('#results');
+    var data = {search: query};
+    return function () {
+      renderNoResultsMsg(data, template, $container);
+    };
+  }
+
+  $searchBtn.on('click', function (evt) {
+    evt.preventDefault();
+    var query = $query.val();
+    query = query.trim();
+    if (helpers.isValidQueryStr(query)) {
+      var qdata = helpers.createOSQueryData(osData, query);
+      var funcs = {
+        hasResults: helpers.hasResults,
+        transformData: helpers.transformOSData,
+        renderResults: renderResultsWrapper,
+        renderNoResultsMsg: renderNoResultsMsgWrapper(query),
+        error: function (status) {console.log(status);}
+      };
+      var osrlf = {
+        success: logFuncs.success(funcs),
+        error: logFuncs.error(funcs)
+      };
+      search(wikiURL, qdata, osrlf);
+
+    } else {
+      console.log('empty query');
+    }
+  });
+
 });
